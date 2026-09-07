78ad49e67ed9e9e1bc2ace0af1f103feeeaa31a6
Pulkit Sharma <pulkit.d.sharma@capgemini.com>
2026-09-07 07:01:32 +0000

    fix(symphony): resolved merge conflicts
    Signed-off-by: Pulkit Sharma <pulkit.d.sharma@capgemini.com>

--- api/pkg/apis/v1alpha1/managers/margo/device.go
+++ api/pkg/apis/v1alpha1/managers/margo/device.go
@@ -428,8 +428,6 @@ func (dm *DeviceManager) OnboardDevice(ctx context.Context, devicePubCert string
 
 func (dm *DeviceManager) ListDevices(ctx context.Context) (margoNonStdAPI.DeviceListResp, error) {
 	devices := margoNonStdAPI.DeviceListResp{
-		ApiVersion: "non.margo.org",
-		Kind:       "DeviceList",
 		Items:      []margoNonStdAPI.DeviceManifestResp{},
 		Metadata:   &margoNonStdAPI.PaginationMetadata{},
 	}
@@ -441,8 +439,6 @@ func (dm *DeviceManager) ListDevices(ctx context.Context) (margoNonStdAPI.Device
 
 	for _, row := range rows {
 		devices.Items = append(devices.Items, margoNonStdAPI.DeviceManifestResp{
-			ApiVersion: "non.margo.org",
-			Kind:       "Device",
 			Metadata: margoNonStdAPI.Metadata{
 				CreationTimestamp: &row.CreatedAt,
 			},
