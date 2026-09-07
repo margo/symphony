a5a1fab8f5f1426a3685c1a5d7112150897c32ae
Pulkit Sharma <pulkit.d.sharma@capgemini.com>
2026-09-07 11:08:55 +0530

    feat(wfm): enhance device capabilities and filtering by appPkgId (#66)
    * feat(cli): adapted list devices to filter based on appPkgId
    
    Signed-off-by: Pulkit Sharma <pulkit.d.sharma@capgemini.com>
    
    * feat(wfm): partially adapted list devices to filter based on appPkgId
    
    Signed-off-by: Pulkit Sharma <pulkit.d.sharma@capgemini.com>
    
    * fix(cli): removed shorthand flag for appPkgId
    
    Signed-off-by: Pulkit Sharma <pulkit.d.sharma@capgemini.com>
    
    * fix(cli): changed shorthand flag for appPkgId
    
    Signed-off-by: Pulkit Sharma <pulkit.d.sharma@capgemini.com>
    
    * fix(wfm): added support of device constraints while deploying
    
    Signed-off-by: Pulkit Sharma <pulkit.d.sharma@capgemini.com>
    
    * fix(wfm): set device constraints while preparing desired state
    
    Signed-off-by: Pulkit Sharma <pulkit.d.sharma@capgemini.com>
    
    * chore(wfm): pointed to latest sandbox
    
    Signed-off-by: Pulkit Sharma <pulkit.d.sharma@capgemini.com>
    
    * chore(symphony): pointed to latest sandbox
    
    Signed-off-by: Pulkit Sharma <pulkit.d.sharma@capgemini.com>
    
    * feat(symphony): added device eligibility check for listing devices for an app
    
    Signed-off-by: Pulkit Sharma <pulkit.d.sharma@capgemini.com>
    
    * chore(symphony): pointed to latest sandbox changes
    
    Signed-off-by: Pulkit Sharma <pulkit.d.sharma@capgemini.com>
    
    * chore(symphony): bumped go version to 1.25.10
    
    Signed-off-by: Pulkit Sharma <pulkit.d.sharma@capgemini.com>
    
    ---------
    
    Signed-off-by: Pulkit Sharma <pulkit.d.sharma@capgemini.com>

--- api/pkg/apis/v1alpha1/vendors/margo/device-mgmt-vendor.go
+++ api/pkg/apis/v1alpha1/vendors/margo/device-mgmt-vendor.go
@@ -9,6 +9,12 @@ import (
 	"github.com/eclipse-symphony/symphony/coa/pkg/apis/v1alpha2/providers/pubsub"
 	"github.com/eclipse-symphony/symphony/coa/pkg/apis/v1alpha2/vendors"
 	"github.com/eclipse-symphony/symphony/coa/pkg/logger"
+	"github.com/kr/pretty"
+	"github.com/margo/sandbox/non-standard/generatedCode/wfm/nbi"
+	"github.com/margo/sandbox/shared-lib/constraints"
+	"github.com/margo/sandbox/shared-lib/pointers"
+	"github.com/margo/sandbox/standard/generatedCode/wfm/sbi"
+
 	"github.com/valyala/fasthttp"
 )
 
@@ -72,11 +78,76 @@ func (self *DeviceMgmtVendor) listDevices(request v1alpha2.COARequest) v1alpha2.
 
 	deviceMgmtVendorLogger.InfofCtx(pCtx, "V (MargoDeviceMgmtVendor): listDevices, method: %s", request.Method)
 
+	appPkgId := request.Parameters["appPackageId"]
 	// Call DeviceManager to list the devices
 	devices, err := self.DeviceManager.ListDevices(pCtx)
 	if err != nil {
 		return createErrorResponse2(deviceMgmtVendorLogger, span, err, "Failed to report device capabilities", v1alpha2.InternalError)
 	}
 
+	if appPkgId == "" {
+		deviceMgmtVendorLogger.InfoCtx(pCtx, "V (MargoDeviceMgmtVendor): appPkgId is empty, returning all devices")
+		return createSuccessResponse(span, v1alpha2.OK, &devices)
+	}
+
+	deviceMgmtVendorLogger.InfofCtx(pCtx, "V (MargoDeviceMgmtVendor): marking devices for appPkgId: %s", appPkgId)
+
+	// getting app package here by appId
+	appPkgRow, err := self.DeviceManager.Database.GetAppPackage(pCtx, appPkgId)
+	if err != nil {
+		deviceMgmtVendorLogger.ErrorfCtx(pCtx, "V (MargoDeviceMgmtVendor): failed to get app package by appPkgId: %s, err: %s", appPkgId, err.Error())
+		return createErrorResponse2(deviceMgmtVendorLogger, span, err, "Failed to get application package for checking eligible devices", v1alpha2.InternalError)
+	}
+
+	deviceMgmtVendorLogger.InfofCtx(pCtx, "V (MargoDeviceMgmtVendor): Printing application package here: %s", pretty.Sprint(appPkgRow))
+
+	dc := constraints.New()
+	for i, d := range devices.Items {
+
+		devCap, err := ConvertAtoB[any, sbi.DeviceCapabilitiesManifest](d.Spec.Capabilities)
+		if err != nil {
+			deviceMgmtVendorLogger.ErrorfCtx(pCtx, "V (MargoDeviceMgmtVendor): failed to convert device capabilities, err: %s, rawCapabilities: %s", err.Error(), pretty.Sprint(d.Spec.Capabilities))
+			return createErrorResponse2(deviceMgmtVendorLogger, span, err, "Failed to convert device capabilities for checking eligible devices", v1alpha2.InternalError)
+		}
+
+		deviceMgmtVendorLogger.InfofCtx(pCtx, "V (MargoDeviceMgmtVendor): printing converted device Capabilities here, : %s", pretty.Sprint(devCap))
+		eligible := true
+		for _, dp := range appPkgRow.AppDescription.DeploymentProfiles {
+
+			if dp.DeviceConstraints == nil {
+				// Deployment profile does not have device constraints present
+				// no need to check anything
+				deviceMgmtVendorLogger.InfoCtx(pCtx, "V (MargoDeviceMgmtVendor): app deployment profile is nil, no need to check")
+				continue
+			}
+
+			devCons, err := ConvertAtoB[nbi.DeviceConstraints, sbi.DeviceConstraints](*dp.DeviceConstraints)
+			if err != nil {
+				deviceMgmtVendorLogger.ErrorfCtx(pCtx, "V (MargoDeviceMgmtVendor): failed to convert device constraints, err: %s, rawDeviceConstraints: %s", err.Error(), pretty.Sprint(*dp.DeviceConstraints))
+				return createErrorResponse2(deviceMgmtVendorLogger, span, err, "Failed to convert device constraints for checking eligible devices", v1alpha2.InternalError)
+			}
+			deviceMgmtVendorLogger.InfofCtx(pCtx, "V (MargoDeviceMgmtVendor): printing converted device constraints here, : %s", pretty.Sprint(devCons))
+
+			ok, reason, err := dc.IsDeviceEligible(devCap, devCons)
+			if err != nil {
+				deviceMgmtVendorLogger.ErrorfCtx(pCtx, "V (MargoDeviceMgmtVendor): failed to check device eligibility, err: %s", err.Error())
+				return createErrorResponse2(deviceMgmtVendorLogger, span, err, "Failed to check for eligible devices", v1alpha2.InternalError)
+			}
+
+			if !ok {
+				eligible = false
+				deviceMgmtVendorLogger.WarnfCtx(pCtx, "V (MargoDeviceMgmtVendor): device is not eligible, checking for next, deviceId : %s, reason: %s", *d.Id, reason)
+				break
+			}
+
+		}
+		if eligible {
+			deviceMgmtVendorLogger.InfoCtx(pCtx, "V (MargoDeviceMgmtVendor): device is eligible, checking for next, deviceId : %s", *d.Id)
+
+			devices.Items[i].Eligible = pointers.Ptr(nbi.True)
+			continue
+		}
+		devices.Items[i].Eligible = pointers.Ptr(nbi.False)
+	}
 	return createSuccessResponse(span, v1alpha2.OK, &devices)
 }
